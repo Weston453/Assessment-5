0a54904788a1bdc955658159c8469d622982c42f
Weston Isheim <wisheim@gmail.com>
2021-09-23 23:21:46 -0600

    added another feature, cleaned up my code, experimented with hitting an api

---
+++ client/main.js
@@ -0,0 +1,81 @@
+// front end js 
+
+// 1) Get a random compliment function FE
+document.getElementById("complimentButton").onclick = function () {
+    axios.get("http://localhost:4000/api/compliment/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        })
+        .catch(err => console.log(err))
+};
+
+
+// 2) Get a random fortune function FE
+document.getElementById("fortuneButton").onclick = function () {
+    axios.get("http://localhost:4000/api/fortune/")
+        .then(function (response) {
+          const data = response.data;
+          alert(data);
+        })
+        .catch(err => console.log(err))
+};
+
+// Extra feature 1) Get a random number function FE
+document.getElementById("randomNumberButton").onclick = function () {
+    axios.get("http://localhost:4000/api/number/")
+    .then(function (response) {
+          const data = response.data;
+          alert(data);
+        })
+        .catch(err => console.log(err))
+};
+
+// Extra feature 2) Guess my number function FE
+
+const guessNumberForm = document.getElementById("guessNumberForm")
+const guessNumberInput = document.getElementById("guessNumberInput")  
+
+guessNumberForm.addEventListener('submit', (event)=> {
+  event.preventDefault()
+  const {number} = guessNumberInput
+  axios.post('http://localhost:4000/guess', number)
+  .then(res => {
+    console.log(res.data)
+  })
+  .catch(err => console.log(err))
+  console.log(event.target.number)
+})
+
+// Extra feature 3) add a fortune to array FE
+const addFortuneForm = document.getElementById('addFortuneForm')
+const fortuneInput = document.getElementById("fortuneInput")
+
+addFortuneForm.addEventListener('submit', (event)=>{
+  event.preventDefault()
+  const {value} = fortuneInput
+  axios.post('http://localhost:4000/api/fortune', {value})
+  .then(res => {
+    console.log(res.data)
+  })
+  .catch(err => console.log(err))
+})
+
+// Extra feature 4) add these numbers FE 
+const addBtn = document.getElementById("addBtn")
+
+addBtn.addEventListener('click', ()=> {
+    axios.get('http://localhost:4000/api/add/')
+      .then(res=>{
+        alert(res.data)
+      })
+      .catch(err => console.log(err))
+})
+    
+// Extra feature 5) display ditto from pokeAPI FE
+// const getPokemon = ()=> {
+//   axios.get('http://localhost:4000/api/pokemon')
+//     .then(res => console.log(res.data))
+// }
+
+// getPokemon()
--- server/index.js
+++ server/index.js
@@ -3,53 +3,82 @@ const cors = require("cors");
 
 const app = express();
 
+const axios = require("axios");
+
 
 app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+// back end js 
+
+// 1) Get a random compliment feature BE
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
 					 "Your Javascript skills are stellar.",
   ];
 
-  // choose random compliment
   let randomIndex = Math.floor(Math.random() * compliments.length);
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
 });
 
-app.get("/api/fortune", (req, res) => {
-  const fortunes = ["A new perspective will come with the new year.", 
-  "A golden egg of opportunity falls into your lap this month.", 
-  "A soft voice may be awfully persuasive.", 
-  "A person of words and not deeds is like a garden full of weeds.", 
-  "Every flower blooms in its own sweet time."
-  ];
+// 2) Get a random fortune feature BE
+const fortunes = ["A new perspective will come with the new year.", 
+"A golden egg of opportunity falls into your lap this month.", 
+"A soft voice may be awfully persuasive.", 
+"A person of words and not deeds is like a garden full of weeds.", 
+"Every flower blooms in its own sweet time."
+];
 
-  let randomValue = Math.floor(Math.random() * fortunes.length)
-  let randomFortune = fortunes[randomValue]
-  res.status(200).send(randomFortune)
+app.get('/api/fortune', (req,res) => {
+
+  const randIndex = Math.floor(Math.random() * fortunes.length)
+  let fortune = fortunes[randIndex]
+  res.status(200).send(fortune)
 });
 
+// Extra feature 1) Get a random number BE
 app.get("/api/number", (req, res) => {
   const array = [1,2,3,4,5,6,7,8,9,10]
   let randomVal = Math.floor(Math.random() * array.length)
   let num = array[randomVal]
-  
   res.status(200).send(`The random number is ${num}`)
-
 });
 
-app.get("/api/guess", (req, res) => {
-  const{input} = req.body
-  if (input.value === "13") {
+// Extra feature 2) Guess my number BE
+app.post("/api/guess", (req, res) => {
+  const {number} = req.body
+  if (number === "13") {
   res.status(200).send("Correct!")
   } else {
-    res.status(200).send("sorry, wrong number")
+    res.status(200).send("Sorry, wrong number")
   } 
+  console.log(number)
 });
+
+// Extra feature 3) add a fortune to array BE
+app.post('/api/fortune', (req, res)=> {
+  const {value} = req.body
+  fortunes.push(value)
+  res.status(200).send(fortunes)
+})
+
+// Extra feature 4) add these numbers BE 
+app.get("/api/add/", (req, res)=> {
+  let solution = (2 + 2)
+  res.status(200).send(`The Answer is ${solution}`)
+});
+
+// Extra feature 5) display ditto from pokeAPI BE
+// app.get('/api/pokemon', (req,res) => {
+//   axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+//   .then(res => {
+//     res.status(200).send(res.data)
+//   })
+//   .catch(err => console.log(err))
+// });
 
 app.listen(4000, () => console.log("Server running on 4000"));
